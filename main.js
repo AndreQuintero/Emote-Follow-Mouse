ee7be246dbe5e1f4c68c49a5e05b115a63f75167
Andre <andre.quintero96@gmail.com>
2020-10-29 22:32:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('body').addEventListener('mousemove', eyeball);
+
+function eyeball(event){
+    let eyes = document.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotaion = (radian * (180 / Math.PI) * -1) + 270;
+        eye.style.transform = `rotate(${rotaion}deg)`;
+    });
+}
